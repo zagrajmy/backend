73c389dd6c937408421730f0142614be86ec964e
hasparus <hasparus@gmail.com>
2020-03-24 11:26:47 +0100

    Dereference symlink in heroku/deploy.sh

--- heroku/deploy.sh
+++ heroku/deploy.sh
@@ -1,3 +1,28 @@
-git subtree split --prefix heroku -b temporary
-git push -f heroku temporary:master
-git branch -D temporary 
+migrations="$(dirname $0)/hasura/migrations"
+branch="$(git rev-parse --abbrev-ref HEAD)"
+
+set +e
+git branch -D temp-deploy-2
+git branch -D temp-deploy-1
+set -e
+
+git checkout -b temp-deploy-1
+
+# git subtree doesn't dereference symlinks
+rm "$migrations-symlink" &> /dev/null
+mv $migrations "$migrations-symlink"
+cp -Lr "$migrations-symlink" $migrations
+rm "$migrations-symlink"
+git add "$(dirname $0)/hasura"
+git \
+  -c user.name='zagrajmy bot' \
+  -c user.email='bot@zagrajmy.net@gmail.com' \
+  commit -m 'chore(auto): copy migrations to heroku'
+
+git subtree split --prefix heroku -b temp-deploy-2
+git push -f heroku temp-deploy-2:master
+
+git checkout $branch
+
+git branch -D temp-deploy-2
+git branch -D temp-deploy-1
