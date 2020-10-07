61c497d1954c61981b36e971e11961b758623357
hasparus <hasparus@gmail.com>
2020-10-07 09:30:32 +0200

    Add ALLOWED_HOSTS to development settings

--- app/zagrajmy/settings/development.py
+++ app/zagrajmy/settings/development.py
@@ -1,5 +1,6 @@
 # pylint: disable=unused-wildcard-import
 from zagrajmy.settings.base import *  # pylint: disable=wildcard-import
+import os
 
 DEBUG = True
 
@@ -12,3 +13,5 @@ INSTALLED_APPS += [
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
